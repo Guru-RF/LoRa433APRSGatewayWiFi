49f9da5108b2c6aedf202846e99c2a71c4dfad81
Joeri Van Dooren <ure@rf.guru>
2024-02-25 20:11:47 +0100

    add syslog log

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# Base config
+# I use papertrail but other services are available
+syslogHost = "logs4.papertrailapp.com"
+syslogPort = 24262
+# APRS
+call = "ON3URE-4"
+aprs_host = "belgium.aprs2.net"
+aprs_port = 14580
+passcode = "23996"  # https://apps.magicbug.co.uk/passcode/index.php/passcode
+latitude = 51.1517227
+longitude = 2.7649157
+altitude = 46  # in meters
+comment = "https://rf.guru LoraAPRSiGate v0.1"
+symbol = "La"
+# Features
+biast = False
+# Gateway only
+msgDistance = 300  # distance in km's from source of messages
